6952b09e97a86ec54c6f4fae3b34a318d645f471
liyann <liyannan99@gmail.com>
2017-03-05 20:14:24 +0800

    完成实现树形控件

--- 百度糯米前端技术学院/tree-view/treeView.js
+++ 百度糯米前端技术学院/tree-view/treeView.js
@@ -17,6 +17,36 @@ let nodes = [{
         }]
     }
 ];
+var nodesTest = [{
+    name: "父节点1",
+    children: [{
+        name: "子节点11"
+    },
+        {
+            name: "子节点12"
+        },
+        {
+            name: "子节点13",
+            children: [{name: "叶子节点131"}]
+        }]
+},
+    {
+        name: "父节点2",
+        children: [{name: "子节点21"},
+            {
+                name: "子节点22",
+                children: [{name: "叶子节点221"},
+                    {name: "叶子节点222", children: [{name: "叶子节点2221"}]}]
+            }]
+    },
+    {
+        name: "父节点3",
+        children: [{name: "子节点31"},
+            {
+                name: "子节点32",
+                children: [{name: "叶子节点321"}]
+            }]
+    }];
 
 let DOMTreeRender = (nodes, DOMNode) => {
     let str = '';
@@ -77,6 +107,6 @@ function exchangeNodeStyle(node, preStyle, curStyle) {
     }
 }
 
-DOMTreeRender(nodes, '#tree');//渲染树形控件
+DOMTreeRender(nodesTest, '#tree');//渲染树形控件
 
 
