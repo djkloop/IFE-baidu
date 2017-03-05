c0dfa9dd059fb11df187f27a7446438b8e036d40
liyann <liyannan99@gmail.com>
2017-03-05 20:03:43 +0800

    完成实现树形控件

---
+++ 百度糯米前端技术学院/tree-view/readme.md
@@ -0,0 +1,2 @@
+1. 对nodes进行先序遍历，将数据转换成DOM树
+2. 图标引用自[fontawesome](http://fontawesome.io/)
---
+++ 百度糯米前端技术学院/tree-view/treeView.js
@@ -0,0 +1,82 @@
+/**
+ * Created by Li on 2017/3/4.
+ */
+window.$ = function (selector) {
+    return document.querySelector(selector);
+};
+
+let nodes = [{
+    name: "父节点1",
+    children: [{name: "子节点1"}, {name: "子节点2"}]
+},
+    {
+        name: "父节点2",
+        children: [{name: "子节点3"}, {
+            name: "父节点3",
+            children: [{name: "子节点5"}]
+        }]
+    }
+];
+
+let DOMTreeRender = (nodes, DOMNode) => {
+    let str = '';
+
+    (function insertNode(data) {
+        if (data.length > 0) {
+            data.map(item => {
+                if (item.children) {
+                    str += `<div class="tree" ><div class="folder" onclick="toggleSlider(event)"><i class="fa fa-minus node-icon"></i><i class="fa fa-folder-open node-icon"></i><a class='folder-a' href='#'>${item.name}</a></div><div class='file-node slide'>`;
+
+                    insertNode(item.children);
+                    str += "</div></div>";
+                } else {
+                    str += `<div class="file"><i class="fa fa-file-o node-icon"></i><a class='file-node'>${item.name}</a></div>`;
+                }
+            })
+        }
+    })(nodes);
+
+    $(DOMNode).innerHTML = str;
+};
+
+/*
+ * 点击文件夹隐藏子文件和子文件夹
+ * */
+function toggleSlider(event) {
+
+    let folderIcon = event.target.previousSibling,
+        plusIcon = folderIcon.previousSibling,
+        /*
+         点击的DOM元素为文件夹节点，
+         获取点击节点的父亲节点的下一个兄弟节点
+         */
+        fileNode = event.target.parentNode.nextSibling;
+
+    exchangeNodeStyle(plusIcon, 'fa-minus', 'fa-plus');//加减图标互换
+    exchangeNodeStyle(folderIcon, 'fa-folder-open', 'fa-folder-o');//文件夹打开关闭图标互换
+
+    if (fileNode.classList.contains('slide-up')) {
+        fileNode.classList.remove('slide-up');
+    } else {
+        fileNode.classList.add('slide-up');
+    }
+}
+
+/*
+ * 交换某个节点的两个CSS样式
+ * perStyle、curStyle必须有一个
+ * */
+function exchangeNodeStyle(node, preStyle, curStyle) {
+
+    if (node.classList.contains(preStyle)) {
+        node.classList.remove(preStyle);
+        node.classList.add(curStyle)
+    } else {
+        node.classList.remove(curStyle);
+        node.classList.add(preStyle)
+    }
+}
+
+DOMTreeRender(nodes, '#tree');//渲染树形控件
+
+
